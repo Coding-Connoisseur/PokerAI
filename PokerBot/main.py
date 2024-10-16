03e5b04a450d8879f79101ff3a8958a07eba18f0
Coding-Connoisseur <davidtraversmailbox@gmail.com>
2024-10-16 18:36:50 +0000

    Refactor Dockerfile and main.py, and update requirements.txt and setup.py

--- PokerBot/main.py
+++ PokerBot/main.py
@@ -1,1 +1,15 @@
-# main.py - Main project file
+
+import argparse
+from core.bot_controller import BotController
+
+def main():
+    parser = argparse.ArgumentParser(description='Start the PokerBot')
+    parser.add_argument('--game_type', type=str, default='TexasHoldem', help='Type of poker game')
+    parser.add_argument('--buy_in', type=float, default=50.0, help='Buy-in amount for the game')
+    args = parser.parse_args()
+
+    bot_controller = BotController()
+    bot_controller.run_bot(game_type=args.game_type, buy_in=args.buy_in)
+
+if __name__ == '__main__':
+    main()
--- PokerBot/setup.py
+++ PokerBot/setup.py
@@ -1,1 +1,24 @@
-# setup.py - Main project file
+from setuptools import setup, find_packages
+
+setup(
+    name='PokerBot',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # Dependencies will be listed in requirements.txt
+    ],
+    entry_points={
+        'console_scripts': [
+            'pokerbot = main:main'
+        ]
+    },
+    description='An AI-powered poker bot for online poker platforms.',
+    author='Your Name',
+    author_email='youremail@example.com',
+    url='https://github.com/yourusername/PokerBot',
+    classifiers=[
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
